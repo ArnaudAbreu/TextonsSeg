c10a1290af9d8d4dae9e8f29776a2ea762b93e25
ABREU ARNAUD <aabreu@unistra.fr>
2020-11-21 22:45:28 +0100

    fresh package architecture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='textons',
+      version='0.1',
+      description='procedures for textons image analysis',
+      author='Arnaud Abreu',
+      author_email='arnaud.abreu.p@gmail.com',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[],
+      include_package_data=True)
